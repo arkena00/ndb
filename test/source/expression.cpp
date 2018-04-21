7d4eec02365b2036cf0fa4c77761083b1e03dcd0
ads00 <adess00@hotmail.fr>
2018-04-21 05:03:22 +0200

    [update] explicit test

--- test/source/expression.cpp
+++ test/source/expression.cpp
@@ -26,6 +26,12 @@ TYPED_TEST(expression, simple)
     EXPECT_NO_THROW( this->q << (movie.id, movie.name, movie.image) );
 }
 
+TYPED_TEST(expression, explicit_keyword)
+{
+    EXPECT_NO_THROW( this->q << ndb::get(movie.id) );
+    EXPECT_NO_THROW( this->q << (ndb::get(movie.id) << ndb::source(movie)) );
+}
+
 TYPED_TEST(expression, function)
 {
     EXPECT_NO_THROW( this->q << (ndb::now(), movie.id, ndb::now()) );
@@ -34,5 +40,6 @@ TYPED_TEST(expression, function)
 
 TYPED_TEST(expression, composed)
 {
+    EXPECT_NO_THROW( this->q << ((ndb::now(), movie.id) << ndb::limit(2)) );
     EXPECT_NO_THROW( this->q << ((ndb::now(), movie.id) << ndb::limit(2)) );
 }
