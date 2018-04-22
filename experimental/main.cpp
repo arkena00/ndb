0bc55c0974e67fe4986565796fefa217535e85a4
ads00 <adess00@hotmail.fr>
2018-04-22 04:05:49 +0200

    [test]

--- experimental/main.cpp
+++ experimental/main.cpp
@@ -13,34 +13,40 @@ int main()
 
     std::stringstream result;
 
-    ndb::initializer<sqlite> init;
-    ndb::connect<dbs::zeta, sqlite>();
+    try
+    {
+        ndb::initializer<sqlite> init;
+        ndb::connect<dbs::zeta, sqlite>();
 
-    int a = 11;
-    double b = 22;
-    auto c = std::chrono::system_clock::now();
+        int a = 11;
+        double b = 22;
+        auto c = std::chrono::system_clock::now();
 
-    const auto& movie = models::library.movie;
-    const auto& user = models::library.user;
+        const auto& movie = models::library.movie;
+        const auto& user = models::library.user;
 
-    ndb::query<dbs::zeta>() << ndb::get(user.id, user.name);
-
-    //auto q = (movie.id << movie);
-    //auto z = ((movie.id, q) << movie);
-
-/*
-    auto z = (movie.id << movie);
-    auto u = ndb::limit(3);
-    ndb::query<dbs::zeta> v;
-    ndb::result<dbs::zeta> res = z << u;
-    v = z << u;
-*/
-
-    ndb::query_view<dbs::zeta> q;
-    q << (movie.id, movie.name, ndb::get(movie.image));
-    std::cout << q.view();
+        ndb::query<dbs::zeta>() << (ndb::get(movie.id) << ndb::source(movie));
 
 
+        //auto q = (movie.id << movie);
+        //auto z = ((movie.id, q) << movie);
+
+    /*
+        auto z = (movie.id << movie);
+        auto u = ndb::limit(3);
+        ndb::query<dbs::zeta> v;
+        ndb::result<dbs::zeta> res = z << u;
+        v = z << u;
+    */
+
+        ndb::query_view<dbs::zeta> q;
+        q << (movie.id, movie.name, ndb::get(movie.image));
+        std::cout << q.view();
+
+    } catch(const std::exception& e)
+    {
+        std::cout << e.what();
+    }
 
     return 0;
 
