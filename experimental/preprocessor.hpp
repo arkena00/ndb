9b68fe61b060269ef10253725c8080685879e9a2
ads00 <adess00@hotmail.fr>
2018-05-03 15:43:49 +0200

    [update] preprocessor tests

--- experimental/preprocessor.hpp
+++ experimental/preprocessor.hpp
@@ -6,6 +6,15 @@
 #include <boost/preprocessor/list/for_each_i.hpp>
 #include <boost/preprocessor/variadic/to_list.hpp>
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////           GENERATION           ////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// declare namespaces
+namespace ndb::tables{}
+namespace ndb::models{}
+namespace ndb::databases{}
+
+
 // common
 // unpack macro
 #define ndb_unpack(ITEM) BOOST_PP_CAT(ndb_unpack_, ITEM)
@@ -16,20 +25,21 @@
 
 #define ndb_unpack_ndb_field(...)  __VA_ARGS__
 
-#define ndb_make_field_impl(PARENT, NAME, TYPE, SIZE) static constexpr struct NAME##_ : ndb::field<PARENT, TYPE, ndb::size<SIZE>> { ndb_field_op } NAME {};
+#define ndb_make_field_impl(PARENT, NAME, TYPE, SIZE) static constexpr struct NAME##_ : ::ndb::field<PARENT, TYPE, ::ndb::size<SIZE>> { ndb_field_op } NAME {};
 #define ndb_make_field_impl_args(data, item) (data, item)
 
 #define ndb_make_field(r, data, item) BOOST_PP_EXPAND(ndb_make_field_impl ndb_make_field_impl_args(data, ndb_unpack(item)) )
 
-#define ndb_table(TABLE_NAME, ...) template<class Model> struct TABLE_NAME : ndb::table<Model> { \
+#define ndb_table(TABLE_NAME, ...) namespace ndb::tables { \
+    template<class Model> struct TABLE_NAME : ::ndb::table<Model> { \
     BOOST_PP_LIST_FOR_EACH( \
         ndb_make_field, \
         TABLE_NAME, \
         BOOST_PP_VARIADIC_TO_LIST(__VA_ARGS__) \
     ) \
-    using Detail_ = ndb::table_detail \
+    using Detail_ = ::ndb::table_detail \
     < \
-        ndb::entity< \
+        ::ndb::entity< \
             BOOST_PP_LIST_FOR_EACH_I( \
                 ndb_make_table_detail_entity, \
                 BOOST_NIL, \
@@ -37,23 +47,25 @@
             ) \
         > \
     >; \
-};
+}; \
+} // ::ndb::tables
 
 // model
 #define ndb_make_model_detail_entity(r, data, i, item) BOOST_PP_COMMA_IF(BOOST_PP_NOT_EQUAL(i, 0)) BOOST_PP_CAT(item, _)
 
-#define ndb_make_table_impl(MODEL_NAME, TABLE_NAME) using TABLE_NAME##_ = TABLE_NAME<MODEL_NAME>; static constexpr TABLE_NAME##_ TABLE_NAME {};
+#define ndb_make_table_impl(MODEL_NAME, TABLE_NAME) using TABLE_NAME##_ = ::ndb::tables::TABLE_NAME< ::ndb::models::MODEL_NAME##_ >; static constexpr TABLE_NAME##_ TABLE_NAME {};
 #define ndb_make_table(r, data, item) ndb_make_table_impl(data, item)
 
-#define ndb_model(MODEL_NAME, ...) struct MODEL_NAME { \
+#define ndb_model(MODEL_NAME, ...) namespace ndb::models { \
+    struct MODEL_NAME##_ { \
     BOOST_PP_LIST_FOR_EACH( \
         ndb_make_table, \
         MODEL_NAME, \
         BOOST_PP_VARIADIC_TO_LIST(__VA_ARGS__) \
     ) \
-    using Detail_ = ndb::model_detail \
+    using Detail_ = ::ndb::model_detail \
     < \
-        ndb::entity< \
+        ::ndb::entity< \
             BOOST_PP_LIST_FOR_EACH_I( \
                 ndb_make_model_detail_entity, \
                 BOOST_NIL, \
@@ -61,28 +73,31 @@
             ) \
         > \
     >; \
-};
+}; \
+static constexpr const ::ndb::models::MODEL_NAME##_ MODEL_NAME = {}; \
+} // ::ndb::models
 
 // database
 #define ndb_make_database_detail_entity(r, data, i, item) BOOST_PP_COMMA_IF(BOOST_PP_NOT_EQUAL(i, 0)) BOOST_PP_CAT(BOOST_PP_VARIADIC_ELEM(0, ndb_unpack(item)), _)
 
 #define ndb_unpack_ndb_database(...)  __VA_ARGS__
 
-#define ndb_make_database_impl(PROJECT_NAME, NAME, MODEL, DB_ENGINE) static constexpr struct NAME##_ : ndb::database<project, MODEL, DB_ENGINE>{} NAME{};
+#define ndb_make_database_impl(PROJECT_NAME, NAME, MODEL_NAME, DB_ENGINE) static constexpr struct NAME##_ : ::ndb::database< ::ndb::databases::PROJECT_NAME, ::ndb::models::MODEL_NAME##_, DB_ENGINE >{} NAME{};
 #define ndb_make_database_impl_args(data, item) (data, item)
 
 #define ndb_make_database(r, data, item) BOOST_PP_EXPAND(ndb_make_database_impl ndb_make_database_impl_args(data, ndb_unpack(item)) )
 
-#define ndb_project(PROJECT_NAME, ...) struct project { \
+#define ndb_project(PROJECT_NAME, ...) namespace ndb::databases { \
+    struct PROJECT_NAME { \
     static constexpr auto name = #PROJECT_NAME; \
     BOOST_PP_LIST_FOR_EACH( \
         ndb_make_database, \
         PROJECT_NAME, \
         BOOST_PP_VARIADIC_TO_LIST(__VA_ARGS__) \
     ) \
-    using Detail_ = ndb::table_detail \
+    using Detail_ = ::ndb::database_detail \
     < \
-        ndb::entity< \
+        ::ndb::entity< \
             BOOST_PP_LIST_FOR_EACH_I( \
                 ndb_make_database_detail_entity, \
                 BOOST_NIL, \
@@ -90,7 +105,8 @@
             ) \
         > \
     >; \
-};
+}; \
+} // ::ndb::databases
 
 
 
