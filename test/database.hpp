991a2b7f8174eb5fae424c07f3ba94e0c91e8b48
ads00 <adess00@hotmail.fr>
2018-05-07 13:12:36 +0200

    [update] database

--- test/database.hpp
+++ test/database.hpp
@@ -12,12 +12,13 @@
 namespace fields
 {
     template<class Table>
-    struct id : public ndb::field<Table, int, ndb::size<sizeof(int)>> { ndb_field_op };
+    struct id : public ndb::field<Table, int, ndb::size<sizeof(int)>> { ndb_internal_field_op };
 }
 
 namespace tables
 {
-    struct user
+    template<class Model>
+    struct user : ndb::table<Model>
     {
         using id_ = fields::id<user>; static constexpr id_ id {};
         struct name_ : ndb::field<user, std::string, ndb::size<255>> {} name;
@@ -31,9 +32,9 @@ namespace tables
     struct movie : ndb::table<Model>
     {
         using id_ = fields::id<movie>; static constexpr id_ id {};
-        static constexpr struct name_ : ndb::field<movie, std::string, ndb::size<255>> { ndb_field_op } name {};
-        static constexpr struct image_ : ndb::field<movie, std::string, ndb::size<255>> { ndb_field_op } image {};
-        static constexpr struct user_ : ndb::field<movie, tables::user> {} user {};
+        static constexpr struct name_ : ndb::field<movie, std::string, ndb::size<255>> { ndb_internal_field_op } name {};
+        static constexpr struct image_ : ndb::field<movie, std::string, ndb::size<255>> { ndb_internal_field_op } image {};
+        static constexpr struct user_ : ndb::field<movie, tables::user<Model>> {} user {};
 
         using Detail_ = ndb::table_detail
         <
@@ -45,8 +46,8 @@ namespace tables
     struct music : ndb::table<Model>
     {
         using id_ = fields::id<music>; static constexpr id_ id {};
-        static constexpr struct name_ : ndb::field<music, std::string, ndb::size<255>> { ndb_field_op } name {};
-        static constexpr struct image_ : ndb::field<music, std::string, ndb::size<255>> { ndb_field_op } image {};
+        static constexpr struct name_ : ndb::field<music, std::string, ndb::size<255>> { ndb_internal_field_op } name {};
+        static constexpr struct image_ : ndb::field<music, std::string, ndb::size<255>> { ndb_internal_field_op } image {};
         //static constexpr struct user_ : ndb::field<movie, tables::user> {} user {};
 
         using Detail_ = ndb::table_detail
@@ -61,11 +62,8 @@ struct models
     struct library_
     {
         using movie_ = tables::movie<library_>; static constexpr movie_ movie {};
-        using user_ = tables::movie<library_>; static constexpr user_ user {};
+        using user_ = tables::user<library_>; static constexpr user_ user {};
         using music_ = tables::music<library_>; static constexpr music_ music {};
-
-        //static constexpr struct movie_ : ndb::table<library, tables::movie> {} movie {};
-        //static constexpr struct sound_ : ndb::table<library, tables::movie> {} sound {};
 
         using Detail_ = ndb::model_detail
         <
