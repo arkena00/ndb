2d2cb24c711cf1593d553db5a59c12a4b27e9249
ads00 <adess00@hotmail.fr>
2018-05-17 04:51:03 +0200

    [idea] tree table

--- experimental/main.cpp
+++ experimental/main.cpp
@@ -7,6 +7,44 @@
 
 #include "database.hpp"
 
+/* tree_table
+
+template<class Table, class Depth>
+ndb::hierarchy_table<Table>
+{
+    static constexpr struct parent_id_ : ndb::field<Table, int> {} parent_id {};
+    static constexpr struct depth_ : ndb::field<Table, int> {} depth {};
+    static constexpr struct path_ : ndb::field<Table, ndb::bytes, ndb::size<Depth>> {} path {};
+}
+struct my_htable : ndb::table<Model>, ndb::hierarchy_table<my_htable, depth<10>>
+{
+    // name
+}
+
+query() << (myhtable.name) << ndb::hierarchy_table::childs(id)
+
+ template<class Table>
+ndb::connection_table<Table>
+{
+    static constexpr struct source_id_ : ndb::field<Table, int> {} source_id {};
+    static constexpr struct target_id_ : ndb::field<Table, int> {} target_id {};
+}
+
+
+
+
+struct my_table : ndb::table<Model>, ndb::connection_table<my_ctable>
+{
+    // id
+    // name
+    static constexpr struct connexions_ : ndb::field<my_table, int> {} connexions {};
+}
+
+
+
+
+ */
+
 int main()
 {
     using ndb::sqlite;
