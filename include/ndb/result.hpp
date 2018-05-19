90cb1a7399198447b66427140b4d5e9ac0c0c8ba
ads00 <adess00@hotmail.fr>
2018-05-19 04:05:49 +0200

    [update] add structured binding

--- include/ndb/result.hpp
+++ include/ndb/result.hpp
@@ -26,30 +26,55 @@ namespace ndb
 
     private:
         std::vector<T> line_list_;
-        int _add_id;
+        int add_id_;
 
     public:
-        result() : _add_id(0) {}
-
-
+        result() : add_id_{ 0 } {}
 
         void add(T l)
         {
             line_list_.push_back(std::move(l));
         }
 
-        int add_id() const { return _add_id; }
+        int add_id() const
+        {
+            return add_id_;
+        }
 
-        size_t size() const { return line_list_.size(); }
+        size_t size() const
+        {
+            return line_list_.size();
+        }
 
-        bool has_result() const { return size() > 0; }
+        bool has_result() const
+        {
+            return size() > 0;
+        }
 
-        T& operator[](int index) { return line_list_.at(index); }
+        T& operator[](int index)
+        {
+            return line_list_.at(index);
+        }
+
+        // structured binding
+        template<int i>
+        auto get()
+        {
+            return line_list_[0];
+        }
 
         iterator begin() { return line_list_.begin(); }
         iterator end() { return line_list_.end(); }
-
     };
 } // ndb
+
+namespace std
+{
+    template<class T, class Engine>
+    struct tuple_size<::ndb::result<T, Engine>> : std::integral_constant<std::size_t, 1> {};
+
+    template<class T, class Engine>
+    struct tuple_element<0, ::ndb::result<T, Engine>> { using type = T; };
+} // std
 
 #endif // RESULT_H_NDB
