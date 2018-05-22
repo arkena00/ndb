a229c0a4a4aca038e744cb9d6228953ac67a5fd1
ads00 <adess00@hotmail.fr>
2018-05-22 05:20:44 +0200

    [update] improve type management

--- include/ndb/engine/sqlite/type.hpp
+++ include/ndb/engine/sqlite/type.hpp
@@ -12,20 +12,50 @@
 
 namespace ndb
 {
+    #define ndb_type_map(NDB_TYPE, CPP_TYPE) \
+    template<class Engine> \
+    struct cpp_type<NDB_TYPE, Engine> { using type = CPP_TYPE; }; \
+    template<class Engine> \
+    struct ndb_type<CPP_TYPE, Engine> { using type = NDB_TYPE; };
+
     class sqlite;
 
-    // associate engine type id to native type
-    template<> struct cpp_type<sqlite, SQLITE_INTEGER> { using type = int; };
-    template<> struct cpp_type<sqlite, SQLITE_FLOAT> { using type = double; };
-    template<> struct cpp_type<sqlite, SQLITE3_TEXT> { using type = std::string; };
-    template<> struct cpp_type<sqlite, SQLITE_BLOB> { using type = std::vector<char>; };
+    // ndb types are proxy types
+    namespace types
+    {
+        struct int_;
+        struct float_;
+        struct double_;
+        struct string_;
+        struct byte_array_;
+    } // types
+    using namespace ::ndb::types;
 
-    // associate native type to engine type id
-    template<> struct type_id<sqlite, int> { static constexpr auto value = SQLITE_INTEGER; };
-    template<> struct type_id<sqlite, double> { static constexpr auto value = SQLITE_FLOAT; };
-    template<> struct type_id<sqlite, std::string> { static constexpr auto value = SQLITE3_TEXT; };
-    template<> struct type_id<sqlite, std::vector<char>> { static constexpr auto value = SQLITE_BLOB; };
+    // associate engine type id to ndb type
+    template<> struct engine_ndb_type<sqlite, SQLITE_INTEGER> { using type = int_; };
+    template<> struct engine_ndb_type<sqlite, SQLITE_FLOAT> { using type = float_; };
+    template<> struct engine_ndb_type<sqlite, SQLITE3_TEXT> { using type = string_; };
+    template<> struct engine_ndb_type<sqlite, SQLITE_BLOB> { using type = byte_array_; };
 
+    // associate ndb type to engine type id
+    template<> struct engine_type_id<sqlite, int_> { static constexpr auto value = SQLITE_INTEGER; };
+    template<> struct engine_type_id<sqlite, double_> { static constexpr auto value = SQLITE_FLOAT; };
+    template<> struct engine_type_id<sqlite, string_> { static constexpr auto value = SQLITE3_TEXT; };
+    template<> struct engine_type_id<sqlite, byte_array_> { static constexpr auto value = SQLITE_BLOB; };
+
+    // associate ndb_type -> cpp_type, cpp_type -> ndb_type
+    ndb_type_map(int_, int);
+    ndb_type_map(float_, float);
+    ndb_type_map(double_, double);
+    ndb_type_map(string_, std::string);
+
+
+    // custom type (non-native)
+    template<class T, class U>
+    struct ndb_type<std::chrono::duration<T, U>, sqlite> { using type = double_; };
+
+
+/*
     // associate custom type to native type
     template<> struct native_type<sqlite, const char*> { using type = std::string; };
     template<class T, class U>
@@ -50,7 +80,7 @@ namespace ndb
         {
             return T{ v };
         };
-    };
+    };*/
 } // ndb
 
 
--- include/ndb/engine/type.hpp
+++ include/ndb/engine/type.hpp
@@ -8,28 +8,38 @@
 namespace ndb
 {
     class value;
+    struct type_undefined;
 
     template<class Engine, class... Ts>
     struct type_list;
 
+    // engine ndb_type / type_id
     template<class Engine, class T>
-    struct type_id;
+    struct engine_type_id;
 
-    template<class Engine, int>
-    struct cpp_type;
+    template<class T, int Type_id>
+    struct engine_ndb_type;
 
-    template<class Engine, class T>
-    struct native_type
-    {
-        using type = T;
-    };
+    // ndb_type / cpp_type
+    template<class Ndb_type, class Engine>
+    struct cpp_type { using type = type_undefined; };
+    template<class Ndb_type, class Engine>
+    using cpp_type_t = typename cpp_type<Ndb_type, Engine>::type;
+
+    template<class Cpp_type, class Engine>
+    struct ndb_type { using type = type_undefined; };
+    template<class Cpp_type, class Engine>
+    using ndb_type_t = typename ndb_type<Cpp_type, Engine>::type;
+
+    // ndb_type from T, then cpp_type
+    template<class T, class Engine>
+    using native_type = cpp_type_t< ndb_type_t<T, Engine>, Engine >;
 
     template<class Engine>
     struct basic_type
     {
-        template<class T> using id = type_id<Engine, T>;
-        template<class T> using native = typename native_type<Engine, T>::type;
-        template<int ID> using get = typename cpp_type<Engine, ID>::type;
+        template<class T> using id = engine_type_id<Engine, T>;
+        template<class T> using native = native_type<T, Engine>;
     };
 
     template<class Engine>
