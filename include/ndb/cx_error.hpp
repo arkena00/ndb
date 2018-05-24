75f5ea3e0ef880b82415b6ee884d2c51a3655a80
ads00 <adess00@hotmail.fr>
2018-05-24 08:03:39 +0200

    [update] add cx_error for types

--- include/ndb/cx_error.hpp
+++ include/ndb/cx_error.hpp
@@ -4,19 +4,11 @@
 // \brief compile time error
 
 #define ncx_error(Source, Err, ...) ndb::cx_error<Source, Err< __VA_ARGS__ > >::value;
+#define ncx_error_type(Source, Err, ...) typename ndb::cx_error<Source, Err< __VA_ARGS__ > >::type;
 
 namespace ndb
 {
     struct cx_err;
-
-    template<class T>
-    struct cx_err_type_map_not_found;
-
-    template<class T, template<class...> class Container>
-    struct cx_err_type_not_found;
-
-    template<class...>
-    struct cx_err_not_implemented;
 
     template<class Source, class Err>
     struct cx_error_occured;
@@ -35,6 +27,22 @@ namespace ndb
     {
         static constexpr auto value = cx_error_occured_tpl<Source, Err>{};
     };
+
+    // cx errors
+    //! no type_map found for Cpp_type, add a type_map specialization
+    template<class Cpp_type, class Engine>
+    struct cx_err_type_map_not_found;
+
+    //! type_map for Cpp_type exists but mapped ndb_type is not found in ndb_types
+    template<class Cpp_type, class Database, class Engine = typename Database::engine, class Group = typename Engine::group>
+    struct cx_err_ndb_type_not_found_for;
+
+
+    template<class T, template<class...> class Container>
+    struct cx_err_type_not_found;
+
+    template<class...>
+    struct cx_err_not_implemented;
 } // ndb
 
 #endif // CX_ERROR_H_NDB
