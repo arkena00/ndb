150f7266dd85cfaa1ff78ea639dcb79d7ea53e9a
ads00 <adess00@hotmail.fr>
2018-06-10 09:34:52 +0200

    [update] improve type management

--- include/ndb/engine/sqlite/query.hpp
+++ include/ndb/engine/sqlite/query.hpp
@@ -42,20 +42,22 @@ namespace ndb
         template<class T>
         void bind_value(const T& value)
         {
-            using ndb_type = ndb_type_t<T>;
-            if constexpr (std::is_same_v<int_, ndb_type>) sqlite3_bind_int(statement_, bind_index_, value);
-            if constexpr (std::is_same_v<int64_, ndb_type>) sqlite3_bind_int64(statement_, bind_index_, value);
-            if constexpr (std::is_same_v<double_, ndb_type>) sqlite3_bind_double(statement_, bind_index_, value);
-            if constexpr (std::is_same_v<string_, ndb_type>) sqlite3_bind_text(statement_, bind_index_, value.c_str(), -1, SQLITE_TRANSIENT); //TODO: use SQLITE_STATIC
-            if constexpr (std::is_same_v<byte_array_, ndb_type>) sqlite3_bind_blob(statement_, bind_index_, value.data(), value.size(), SQLITE_TRANSIENT);
+            using storage_type = ndb::storage_type_t<typename Database::engine, ndb_type_t<T, Database>>;
+
+            if constexpr (std::is_same_v<int_, storage_type>) sqlite3_bind_int(statement_, bind_index_, value);
+            else if constexpr (std::is_same_v<int64_, storage_type>) sqlite3_bind_int64(statement_, bind_index_, value);
+            else if constexpr (std::is_same_v<double_, storage_type>) sqlite3_bind_double(statement_, bind_index_, value);
+            else if constexpr (std::is_same_v<string_, storage_type>) sqlite3_bind_text(statement_, bind_index_, value.c_str(), -1, SQLITE_TRANSIENT); //TODO: use SQLITE_STATIC
+            else if constexpr (std::is_same_v<byte_array_, storage_type>) sqlite3_bind_blob(statement_, bind_index_, value.data(), value.size(), SQLITE_TRANSIENT);
+            else ncx_error(sqlite_query, cx_err_type_unknown, storage_type); // type unknown, add a custom type or use an engine type
             bind_index_++;
         }
 
         template<class T>
         void bind(const T& value)
         {
-            if constexpr (ndb::is_native_type_v<ndb::sqlite, T>) bind_value(value);
-            else bind_value(ndb::custom_type<T, Database>::encode(value)); //check encoders if you have an error here
+            if constexpr (ndb::is_custom_type_v<T, Database>) bind_value(ndb::custom_type<T, Database>::internal_encode(value));
+            else bind_value(value);
         };
 
         template<class Result_type = ndb::line<Database>>
