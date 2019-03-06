905260c72b0566a0fa1663ded9d8ea41242e3de8
ads00 <adess00@hotmail.fr>
2019-03-06 20:33:49 +0100

    [add] functional expression form

--- include/ndb/expression/form.hpp
+++ include/ndb/expression/form.hpp
@@ -10,7 +10,7 @@
 
 namespace ndb
 {
-    enum class expression_forms { none, scalar, brace, op_args, a_op_b, op_a_b };
+    enum class expression_forms { none, scalar, brace, op_args, functional_args, a_op_b, op_a_b };
 
     template<expression_forms Form, class Engine, expression_categories Category = Engine::expr_category>
     struct expression_form;
@@ -78,6 +78,19 @@ namespace ndb
             ne.append(expression_code<Type, Engine, Category>::value);
             T1::template make<Engine>(ne);
             T2::template make<Engine>(ne);
+        }
+    };
+
+    template<class Engine, expression_categories Category>
+    struct expression_form<expression_forms::functional_args, Engine, Category>
+    {
+        template<class Type, class Native_expression, class... Args>
+        static constexpr void make(Native_expression& ne)
+        {
+            ne.append(expression_code<Type, Engine, Category>::value);
+            ne.append('(');
+            (std::decay_t<Args>::template make<Engine>(ne), ...);
+            ne.append(')');
         }
     };
 } // ndb
