3c52a89250da53a7e5c8884dd7dfc88d6f02cf62
ads00 <adess00@hotmail.fr>
2019-05-02 13:30:59 +0200

    [add] id system

---
+++ include/ndb/id.hpp
@@ -0,0 +1,58 @@
+#ifndef ID_H_NDB
+#define ID_H_NDB
+
+#include <ndb/database.hpp>
+#include <ndb/table.hpp>
+#include <ndb/field.hpp>
+
+#include <ndb/utility.hpp>
+#include <ndb/utility/cx_string.hpp>
+
+namespace ndb
+{
+    namespace internal
+    {
+        template<template<class...> class T>
+        struct id;
+
+        template<> struct id<ndb::database> { static constexpr auto value = 'D'; };
+        template<> struct id<ndb::table> { static constexpr auto value = 'T'; };
+        template<> struct id<ndb::field> { static constexpr auto value = 'F'; };
+    } // internal
+
+    template<template<class...> class T>
+    static constexpr auto id = internal::id<T>::value;
+
+    template<class Database>
+    constexpr unsigned char database_id = index_of<std::decay_t<Database>, typename std::decay_t<Database>::group::Detail_::entity>::value;
+    template<class Table>
+    constexpr unsigned char table_id = index_of<std::decay_t<Table>, typename std::decay_t<Table>::model::Detail_::entity>::value;
+    template<class Field>
+    constexpr unsigned char field_id = index_of<std::decay_t<Field>, typename std::decay_t<Field>::table::Detail_::entity>::value;
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////             NAMES              ////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+    template<class Database>
+    constexpr const char* database_name = ndb::cx_string<ndb::id<ndb::database>, ndb::database_id<std::decay_t<Database>>>;
+    template<class Table>
+    constexpr const char* table_name = ndb::cx_string<ndb::id<ndb::table>, ndb::table_id<std::decay_t<Table>>>;
+    template<class Field>
+    constexpr const char* field_name = ndb::cx_string<ndb::id<ndb::field>, ndb::field_id<std::decay_t<Field>>>;
+
+    template<class T>
+    const char* name()
+    {
+        if constexpr (ndb::is_field<T>) return ndb::field_name<T>;
+        else if constexpr (ndb::is_table<T>) return ndb::table_name<T>;
+        else if constexpr (ndb::is_database<T>) return ndb::database_name<T>;
+    }
+
+    template<class T>
+    const char* name(const T&)
+    {
+        return name<T>();
+    }
+} // ndb
+
+#endif // ID_H_NDB
